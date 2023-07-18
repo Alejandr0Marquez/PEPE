f81e00bbeab18038c1e3253d562ccaf77b706d4b
Alejandr0Marquez <94418140+Alejandr0Marquez@users.noreply.github.com>
2023-07-18 12:24:23 -0600

    otro

--- mail.php
+++ mail.php
@@ -20,9 +20,9 @@ $pdf->SetFont('Arial','B',16);
 while ($row = $sql->fetch_assoc()) {
     $pdf->MultiCell(160,10,strval('Marca: '.$row['Marca_C'].'   '.'Modelo: '.$row['Modelo_C'].'   '.'Precio: $ '.$row['Precio_C']), 1, 'L');
  }
-$pdfdoc = $pdf->Output("./docs/Doc","S");
+$pdfdoc = $pdf->Output("Doc","S");
 $pdflisto = chunk_split(base64_encode($pdfdoc));
-$pdflisto->Output('F', 'docs/Nota.pdf');
+$pdfcarpeta = $pdf->Output("./docs/Doc.pdf", "F");
 try {
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->isSMTP();
