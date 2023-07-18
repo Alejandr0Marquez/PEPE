7f9ba5711da735b6aa1c105f934660749417cc3a
Alejandr0Marquez <94418140+Alejandr0Marquez@users.noreply.github.com>
2023-07-18 12:03:41 -0600

    otro

--- mail.php
+++ mail.php
@@ -22,7 +22,7 @@ while ($row = $sql->fetch_assoc()) {
  }
 $pdfdoc = $pdf->Output("Doc","S");
 $pdflisto = chunk_split(base64_encode($pdfdoc));
-
+$pdflisto->Output('./archivos/Nota.pdf', 'F');
 try {
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->isSMTP();
