b4790a70e23e5762b80157a9f8593d0af13f2f54
Alejandr0Marquez <94418140+Alejandr0Marquez@users.noreply.github.com>
2023-07-19 11:08:22 -0600

    otro

--- Carrito.php
+++ Carrito.php
@@ -17,9 +17,9 @@ $sql = mysqli_query($con, "SELECT * FROM carrito");
     <header>
         <div class="Navbar">
             <ul class="BarraP">
-                <li><img src="./img/Tenki.png" alt="Tenki-Logo">/li>
-                <li><a href="./Productos.php">Productos</a></li>
-                <li><a href="./Ubicacion.html">Ubicacion</a></li>
+            <li class="center"><img src="./img/Tenki.png" alt="Tenki-Logo">/li>
+                <li class="center"><a href="./Productos.php">Productos</a></li>
+                <li class="center"><a href="./Ubicacion.html">Ubicacion</a></li>
             </ul>
         </div>
     </header>
